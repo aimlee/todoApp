959559ac65425ff8c6877c484e439f429d377c11
aimlee <ali86144@gmail.com>
2021-06-29 16:26:28 +0600

    first commit

---
+++ components/ToDoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="to-do-container">
+    <div class="create-container">
+      <input v-model="titleOfNewToDo" type="text">
+      <button @click="create">
+        Add
+      </button>
+    </div>
+    <div class="to-do-list-container">
+      <to-do-list-item v-for="todo in todos" :key="todo.id" :todo="todo" v-on:delete="deleteItem" />
+    </div>
+    <div class="to-do-list-container">
+    </div>
+  </div>
+</template>
+
+<script>
+import ToDoItemCreate from '~/components/ToDoItemCreate.vue'
+import ToDoListItem from '~/components/ToDoListItem.vue'
+
+export default {
+  components: {
+    ToDoItemCreate,
+    ToDoListItem
+  },
+  props: {
+    todos: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    createItem (item) {
+      this.todos.push(item)
+    },
+    deleteItem (item) {
+      this.todos.splice(this.todos.indexOf(item), 1)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      My To Do List
+    </h1>
+    <to-do-list :todos="todos" />
+  </div>
+</template>
+<script>
+import ToDoList from '~/components/ToDoList.vue'
+export default {
+components: {
+ToDoList
+},
+data () {
+    return {
+      todos: {
+        type: Array
+      }
+    }
+  },
+  asyncData (ctx) {
+    return {
+      todos: [
+        {
+          'id': '1',
+          'title': 'A',
+          'completed': false
+        },
+        {
+          'id': '2',
+          'title': 'B',
+          'completed': true
+        }
+      ]
+    }
+  }
+}
+</script>
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+  "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 64px;
+  color: #35495e;
+  letter-spacing: 1px;
+  margin-top: 20px;
+  text-align: center;
+}
+
+</style>
